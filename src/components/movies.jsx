b12f50771bdb7a3dec0d6b04154c637dee61ef28
shahsohamk <shahsohamk@gmail.com>
2019-03-31 12:53:19 +0530

    deleteing movies

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,24 +4,39 @@ class Movies extends Component {
   state = {
     movies: getMovies()
   };
+
+  handleDelete = movie => {
+    console.log(movie);
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
   render() {
     return (
       <table className="table">
         <thead>
-          <tr>
+          <tr key>
             <th>Title</th>
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th />
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map(movie => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
